7210585c01714ab6569e828157d8a8e2ab3e23d4
Rafael Albuquerque <61889057+rafaelsalb@users.noreply.github.com>
2023-03-16 18:07:14 -0300

    Add files via upload

--- bubble_sort/script.js
+++ bubble_sort/script.js
@@ -1,3 +1,4 @@
+let amt_bars;
 let canvas;
 let bars = [];
 let idx = 1;
@@ -13,7 +14,8 @@ function start()
 {
     canvas = createCanvas(Utils.elementWidth(parent_div), 600);
     canvas.parent("canvas");
-    let amt_bars = parseInt(document.getElementById("length").value);
+    amt_bars = parseInt(document.getElementById("length").value);
+    amt_bars = constrain(amt_bars, 1, 20000);
     bar_width = width / amt_bars;
     bar_height = height / amt_bars;
     bars = [];
@@ -22,14 +24,14 @@ function start()
     idx = 0;
 
     for (let i = 1; i <= amt_bars; ++i) {
-        bars.push(lerp(1, amt_bars, i / amt_bars));
+        bars.push(lerp(0, amt_bars, i / amt_bars));
     }
 
     let _case = document.getElementById("case").value;
 
     switch (_case) {
         case "random":
-            bars = fisher_yates(bars);
+            bars = Utils.fisher_yates(bars);
             break;
         case "worst":
             bars = bars.reverse();
@@ -44,23 +46,6 @@ function start()
     stop_timer();
     start_timer();
     document.getElementById("average-case").innerText += amt_bars * amt_bars + " iterations.";
-}
-
-function is_sorted()
-{
-    return bars.reduce((n, item) => n !== false && item >= n && item)
-}
-
-function fisher_yates(array)
-{
-    for (let i = array.length - 1; i > 0; --i) {
-        let j = int(random(0, i))
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-
-    return array;
 }
 
 function setup()
@@ -81,7 +66,7 @@ function draw()
 
     if (run) {
         document.getElementById("iterations").innerText = ++iterations;
-        
+
         for(let i = 0; i < bars.length; ++i) {
             rect(i * bar_width, height - bars[i] * bar_height, bar_width, bars[i] * bar_height);
         }
@@ -101,7 +86,7 @@ function draw()
             noLoop();
             stop_timer();
         }
-        if (is_sorted()) {
+        if (Utils.is_sorted(bars)) {
             end = true;
         }
     }
--- bubble_sort/utils.js
+++ bubble_sort/utils.js
@@ -14,4 +14,21 @@ class Utils {
             parseFloat(window.getComputedStyle(element, null).getPropertyValue("padding-bottom"))
         )
     }
+    
+    static is_sorted(arr)
+    {
+        return arr.reduce((n, item) => n !== false && item >= n && item)
+    }
+
+    static fisher_yates(array)
+    {
+        for (let i = array.length - 1; i > 0; --i) {
+            let j = int(random(0, i))
+            let temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+        return array;
+    }
 }

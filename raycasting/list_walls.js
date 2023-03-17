5e08bd790082ae916afe0333b831fac63ff3d4a8
Rafael Albuquerque <61889057+rafaelsalb@users.noreply.github.com>
2023-03-17 15:31:54 -0300

    Add files via upload

--- raycasting/list_walls.js
+++ raycasting/list_walls.js
@@ -8,12 +8,12 @@ function list_walls()
         list += "}]' ";
         list += 'id="wall-' + i.toString() + '">';
         list += i.toString() + ':';
-        list += '<input id="wall-' + i.toString() + '-x1" placeholder="X1" type="text" value="' + walls[i].a.x.toString() + '" style="width: 20px"> ';
-        list += '<input id="wall-' + i.toString() + '-y1" placeholder="Y1" type="text" value="' + walls[i].a.y.toString() + '" style="width: 20px"> ';
-        list += '<input id="wall-' + i.toString() + '-x2" placeholder="X2" type="text" value="' + walls[i].b.x.toString() + '" style="width: 20px"> ';
-        list += '<input id="wall-' + i.toString() + '-y2" placeholder="Y2" type="text" value="' + walls[i].b.y.toString() + '" style="width: 20px"> ';
+        list += '<input id="wall-' + i.toString() + '-x1" placeholder="X1" type="text" value="' + walls[i].a.x.toString() + '" style="width: 25px"> ';
+        list += '<input id="wall-' + i.toString() + '-y1" placeholder="Y1" type="text" value="' + walls[i].a.y.toString() + '" style="width: 25px"> ';
+        list += '<input id="wall-' + i.toString() + '-x2" placeholder="X2" type="text" value="' + walls[i].b.x.toString() + '" style="width: 25px"> ';
+        list += '<input id="wall-' + i.toString() + '-y2" placeholder="Y2" type="text" value="' + walls[i].b.y.toString() + '" style="width: 25px"> ';
         list += 'Cor: ' + '<input id="wall-' + i.toString() + '-color" type="color" value="#FFFFFF" style="width: 35px; height: 25px;"> ';
-        list += 'Altura (cm): ' + '<input id="wall-' + i.toString() + '-height" type="text" value="' + walls[i].height + '" style="width: 20px"> ';
+        list += 'Altura (cm): ' + '<input id="wall-' + i.toString() + '-height" type="text" value="' + walls[i].height + '" style="width: 25px"> ';
         list += '<input type="button" value="Update" onclick="update_wall(' + i.toString() + ')">';
         list += '<input type="button" value="Remove" onclick="remove_wall(' + i.toString() + ')">';
         list += '</div>';

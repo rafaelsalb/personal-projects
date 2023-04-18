483bbe6d183a5b5d0482bb8980afd37ca40d0651
Rafael Albuquerque <61889057+rafaelsalb@users.noreply.github.com>
2023-04-18 20:22:55 -0300

    Add files via upload
    rebuilt the memory building to be based on memory blocks

---
+++ cache_mapping/cache_mapping/global.js
@@ -0,0 +1,8 @@
+const cell_width = 64;
+const cell_height = 32;
+const mem_capacity_Bytes = 64;
+const size_of_block = 4;
+const cache_capacity_Bytes = 8;
+
+const mapping_methods = ["direct", "fully_associative"];
+const mapping_policies = ["random", "LFU", "LRU"];
